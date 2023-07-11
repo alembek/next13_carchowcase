0eaa3dec005be824a51a926c5989ad3b71e63692
Alembek Assaubayev <alem.assaubayev@gmail.com>
2023-07-11 23:53:59 +0900

    first commit

---
+++ utils/index.ts
@@ -0,0 +1,64 @@
+import { CarProps,} from "@/types";
+
+export async function fetchCars(){
+    const headers = {
+    "X-RapidAPI-Key": "cb8171aad3mshff849fb88cb624fp121837jsna58c7f981912",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  }
+
+  const response = await fetch( 
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+  {
+    headers:headers,
+  });
+
+  const result = await response.json()
+
+  return result
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append(
+    "customer",
+    process.env.NEXT_PUBLIC_IMAGIN_API_KEY || ""
+  );
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
+}; 
+
+export const deleteSearchParams = (type: string) => {
+  // Set the specified search parameter to the given value
+  const newSearchParams = new URLSearchParams(window.location.search);
+
+  // Delete the specified search parameter
+  newSearchParams.delete(type.toLocaleLowerCase());
+
+  // Construct the updated URL pathname with the deleted search parameter
+  const newPathname = `${window.location.pathname}?${newSearchParams.toString()}`;
+
+  return newPathname;
+};
